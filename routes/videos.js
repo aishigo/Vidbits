aecb55a8893add16532701065c8def60cc7238ee
kishigo <kelvin.x.ishigo-.ND@disney.com>
2018-03-04 20:58:31 -0800

    Cleanup logging

--- routes/videos.js
+++ routes/videos.js
@@ -2,8 +2,6 @@ const router = require('express').Router();
 const Video = require('../models/video');
 
 router.post('/videos', async (req, res, next) => {
-	console.log('XXXX POST videos');
-	console.log(req.body);
 	const {title, description} = req.body;
 	await Video.create({title, description});
 	res.sendStatus(201);
