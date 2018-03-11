8770709f47ed4e8e5b736a308523783cab944602
kishigo <kelvin.ishigo@disney.com>
2018-03-11 12:33:24 -0700

    Step 16

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-	res.render('index', {title: 'Express'});
-});
+// router.get('/', function (req, res, next) {
+// 	res.render('index', {title: 'Express'});
+// });
 
 router.get('/hello', (req, res, next) => {
 	console.log('hello');
 	res.render('hello.handlebars', {foo: 'WTF'});
 });
 
-router.get('/videos', (req, res, next) => {
-	console.log('GET videos');
-	res.render('hello.handlebars', {foo: 'videos'});
-});
+// router.get('/videos', (req, res, next) => {
+// 	console.log('GET videos');
+// 	res.render('hello.handlebars', {foo: 'videos'});
+// });
 
 module.exports = router;
--- routes/videos.js
+++ routes/videos.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 const Video = require('../models/video');
 
+router.get('/', (req, res, next) => {
+	console.log('landing page handlebars');
+	res.render('index.handlebars');
+});
+
 router.post('/videos', async (req, res, next) => {
+	console.log('render show.handlebars');
 	const {title, description} = req.body;
 	const createdVideo = await Video.create({title, description});
 	res.status(201);
